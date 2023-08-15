4ce9a7ac16a6ef13d92853ea09630223c4e2cdbe
pawarisa <pawarisasangha15@gmail.com>
2023-08-15 22:11:53 +0700

    hw03-3

--- Homework/HW03/hw03-3-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-3-codex-and-fix-bug.cpp
@@ -6,13 +6,14 @@ int main() {
     int i, j, k;
  
 printf("Enter Matrix1 : \n");
+scanf("%d",Matrix1);
 printf("Enter number rows and columns : ");
 scanf("%d %d", &rows1, &cols1);
 for(i=0;i<rows1;i++);
     for(j=0;j<cols1;j++){
-    print("Enter [%d %d] value : ", i , j );
+    printf("Enter [%d %d] value : ", i , j );
     scanf("%d", Matrix1[i][j]);
-}}
+}
  
     printf("\nEnter Matrix2 : \n");
     printf("Enter number rows and columns : ");
@@ -20,43 +21,43 @@ for(i=0;i<rows1;i++);
     for(i=0;i<rows2;i++){
     for(j=0;j<cols2;j++){
     printf("Enter [%d %d] value : ", &i , &j );
-    Scanf("%d",&Matrix2[i][j]);
+    scanf("%d",&Matrix2[i][j]);
     }
     }
  
-    printf("\nMatrix1 : \n")
+    printf("\nMatrix1 : \n");
     for(i=0;i<rows1;i++){
-    for(j=0;j<Cols1;j++){
-    printf("%d ", Matrix1[i][j]):
+    for(j=0;j<cols1;j++){
+    printf("%d ", Matrix1[i][j]);
     }
 printf("\n");
 }
  
-print("\nMatrix2 : \n"):
+printf("\nMatrix2 : \n");
 for(i=0;i<rows2;i++){
     for(j=0;j<cols2;j++){
     printf("%d ", Matrix2[i][j]);
 }
-    printf("\n"):
+    printf("\n");
     }
 
-    rows3 = Cols1;
-    cols3 = rows1:
-    for(i=0;i<Rows3;i++){
+    rows3 = cols1;
+    cols3 = rows1;
+    for(i=0;i<rows3;i++){
     for(j=0;j<cols3;j++)
 Matrix3[i][j] = Matrix1[j][i];
 }
-    }
+    
     printf("\nTranspose of Matrix1 is : \n");
-    for(i=0;i<rows3;I++){
+    for(i=0;i<rows3;i++){
     for(j=0;j<cols3;j++){
-printf("%d ", matrix3[i][j]):
+printf("%d ", Matrix3[i][j]);
     }
-Printf("\n"):
+printf("\n");
     }
     
     if(cols1!=rows2)
-        pintf("\nMatrix1 and Matrix2 connot multiply\n");
+        printf("\nMatrix1 and Matrix2 connot multiply\n");
     else {   
         rows4 = rows1;
     cols4 = cols2;
